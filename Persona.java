11a3726750b4f27530d637a84d7b033b9b5b1398
xjose999 <josejoestar80@gmail.com>
2026-05-30 02:35:57 -0500

    Ejercicio 1.2

---
+++ Persona.java
@@ -0,0 +1,10 @@
+class Persona implements Hablador, Trabajador {
+
+    public void hablar() {
+        System.out.println("Hola, estoy hablando.");
+    }
+
+    public void trabajar() {
+        System.out.println("Estoy trabajando en mi jornada laboral.");
+    }
+}
