92773eaaa25cd12cbef4b556c9bdc93b836cc9c0
xjose999 <josejoestar80@gmail.com>
2026-05-30 02:41:26 -0500

    Ejercicio 2.2

---
+++ Animal.java
@@ -0,0 +1,10 @@
+class Animal implements Volador, Nadador {
+
+    public void volar() {
+        System.out.println("El animal está volando por el aire.");
+    }
+
+    public void nadar() {
+        System.out.println("El animal está nadando bajo el agua.");
+    }
+}
