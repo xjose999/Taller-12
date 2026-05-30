ba290ea212475061f8a51cb55d393f22de5766b5
xjose999 <josejoestar80@gmail.com>
2026-05-30 02:36:33 -0500

    Ejercicio 1.3

---
+++ PruebaInterfaces.java
@@ -0,0 +1,8 @@
+// 4. Clase de prueba para instanciar y usar los métodos
+public class PruebaInterfaces {
+    public static void main(String[] args) {
+        Persona p = new Persona();
+        p.hablar();
+        p.trabajar();
+    }
+}
